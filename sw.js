2781a0563698e2389fc75bcd716c607fcbfaf6ca
Davilindo77 <davi.sousa24@aluno.educa.go.gov.br>
2026-03-13 15:29:48 -0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,23 @@
+const CACHE_NAME = 'notas-v1';
+// Lista de arquivos que o app vai salvar para funcionar sem internet
+const ASSETS = [
+  '/bloco-de-notas-/',
+  '/bloco-de-notas-/index.html',
+  '/bloco-de-notas-/manifest.json'
+];
+
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('notas-v1').then((cache) => cache.addAll(['/bloco-de-notas/', 'index.html']))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
   );
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
   );
 });
